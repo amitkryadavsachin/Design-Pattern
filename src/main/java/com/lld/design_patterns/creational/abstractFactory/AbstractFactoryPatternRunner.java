12362ee2aa35a8a0f30817ecad572e13f4b77a28
amitkryadavsachin <79715261+amitkryadavsachin@users.noreply.github.com>
2024-12-31 15:29:41 +0530

    Abstract Design Pattern Implementation

--- src/main/java/com/lld/design_patterns/creational/abstractFactory/AbstractFactoryPatternRunner.java
+++ src/main/java/com/lld/design_patterns/creational/abstractFactory/AbstractFactoryPatternRunner.java
@@ -1,5 +1,8 @@
 package com.lld.design_patterns.creational.abstractFactory;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AbstractFactoryPatternRunner {
 
     public void run() {
